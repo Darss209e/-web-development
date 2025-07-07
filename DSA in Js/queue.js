950f3728149d1c1343279981a1ba76c041d96136
Darshan <darss209e@gmail.com>
2025-07-07 17:35:28 +0530

    new

--- DSA in Js/queue.js
+++ DSA in Js/queue.js
@@ -12,11 +12,11 @@ function queue() {
     this.front = function () {
         return collection[0];
     }
-    this.size = function(){
+    this.size = function () {
         return collection.length;
     }
-    this.isEmpty = function(){
-        return (collection.length ===0);
+    this.isEmpty = function () {
+        return (collection.length === 0);
     }
 }
 
@@ -30,45 +30,45 @@ console.log(q.front());
 q.print();
 
 
-  function PriorityQueue () {
+function PriorityQueue() {
     var collection = [];
-    this.printCollection = function() {
-      (console.log(collection));
+    this.printCollection = function () {
+        (console.log(collection));
     };
-    this.enqueue = function(element){
-        if (this.isEmpty()){ 
+    this.enqueue = function (element) {
+        if (this.isEmpty()) {
             collection.push(element);
         } else {
             var added = false;
-            for (var i=0; i<collection.length; i++){
-                 if (element[1] < collection[i][1]){ //checking priorities
-                    collection.splice(i,0,element);
+            for (var i = 0; i < collection.length; i++) {
+                if (element[1] < collection[i][1]) { //checking priorities
+                    collection.splice(i, 0, element);
                     added = true;
                     break;
                 }
             }
-            if (!added){
+            if (!added) {
                 collection.push(element);
             }
         }
     };
-    this.dequeue = function() {
+    this.dequeue = function () {
         var value = collection.shift();
         return value[0];
     };
-    this.front = function() {
+    this.front = function () {
         return collection[0];
     };
-    this.size = function() {
-        return collection.length; 
+    this.size = function () {
+        return collection.length;
     };
-    this.isEmpty = function() {
-        return (collection.length === 0); 
+    this.isEmpty = function () {
+        return (collection.length === 0);
     };
 }
 
-var pq = new PriorityQueue(); 
-pq.enqueue(['Beau Carnes', 2]); 
+var pq = new PriorityQueue();
+pq.enqueue(['Beau Carnes', 2]);
 pq.enqueue(['Quincy Larson', 3]);
 pq.enqueue(['Ewa Mitulska-Wójcik', 1])
 pq.enqueue(['Briana Swift', 2])
