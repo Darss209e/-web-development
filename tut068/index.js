77bbe5c2b50a27d1164f9f98449389174531c4ba
Darshan <darss209e@gmail.com>
2025-07-08 15:42:45 +0530

    new

--- tut068/index.js
+++ tut068/index.js
@@ -3,4 +3,14 @@ console.log("hello");
 // let box = document.getElEmentsByClassName("box");
 // console.log(box)
 
-// box[2].style.backgroundcolor="red";
+// box[2].style.backgroundcolor = "red";
+// document.getElementsByClassName("box").style.backgroundcolor="red"  
+
+// document.getElementBy.box")
+
+document.querySelector(".box").style.backgroundColor = "green"; 
+// console.log(document.querySelectorAll(".box"));
+
+// document.querySelectorAll(".box").forEach(e=>{
+//     e.style.backgroundColor="green";
+// })
---
+++ tut069/script.js
@@ -0,0 +1,12 @@
+let a = 6
+
+function facfor(number) {
+    let fac =1;
+    for (let index = 1; index <= number; index++) {
+        fac= fac*index
+        
+    }
+    return fac;
+}
+
+console.log(factorial(a))
